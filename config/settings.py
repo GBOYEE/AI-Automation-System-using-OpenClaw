60d1ba12c134aa6b464fad3e0c82dd1740f216da
root <root@vmi3175689.contaboserver.net>
2026-04-04 09:03:45 +0200

    GSD: Change default task queue path to data/tasks.db (relative) for portability

--- config/settings.py
+++ config/settings.py
@@ -6,4 +6,4 @@ settings = Settings()
 # Autonomous service settings
 settings.operator_mode = os.getenv("OPERATOR_MODE", "cli")
 settings.operator_api_url = os.getenv("OPERATOR_API_URL", "http://localhost:8001")
-settings.task_queue_path = os.getenv("TASK_QUEUE_PATH", "/data/tasks.db")
+settings.task_queue_path = os.getenv("TASK_QUEUE_PATH", "data/tasks.db")
